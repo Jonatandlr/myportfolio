42743a6e6e4ab12c847d7640d4c0a3b4ed9d95dc
Jonatandlr <A01735993@tec.mx>
2024-04-26 10:17:55 -0600

    Add analytics component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en" className='scroll-smooth'>
       <body className={inter.className}>     
         {children}
+        <Analytics />
       </body>
     </html>
   )
